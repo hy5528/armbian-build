92b6282ee00fe294dc80e5537d8281a0904c70e7
hy5528 <95360525+hy5528@users.noreply.github.com>
2024-12-30 22:10:56 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -16,7 +16,7 @@ EOF
 # 更新软件包
 sudo apt-get update
 # 安装 Docker
-sudo apt-get install -y ca-certificates curl gnupg lsb-release 
+sudo apt-get install -y ca-certificates curl gnupg lsb-release alsa-utils
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
